b13ef86f4e9e43f091528e1584c0e42412fd61b8
Bartosz Błaszków <bartosz.blaszkow@swmansion.com>
2019-06-05 13:40:28 +0200

    Bump version to v0.1.1

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.Caps.Audio.FLAC.Mixfile do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.1.1"
   @github_url "https://github.com/membraneframework/membrane-caps-audio-flac"
 
   def project do
