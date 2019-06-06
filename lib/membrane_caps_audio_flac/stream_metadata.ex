d2c1ddf7cce15e28603c79497da7797e4e78213e
Bartosz Błaszków <bartosz.blaszkow@swmansion.com>
2019-06-06 18:29:07 +0200

    Add stream_metadata skeleton

---
+++ lib/membrane_caps_audio_flac/stream_metadata.ex
@@ -0,0 +1,23 @@
+defmodule Membrane.Caps.Audio.FLAC.StreamMetadata do
+  @moduledoc """
+  Module defining metadata struct for FLAC buffers with stream metadata.
+
+  Based on contents of [stream before audio frames](https://xiph.org/flac/format.html#frame_header)
+  """
+
+  @type t() :: %__MODULE__{
+          type:
+            :marker
+            | :streaminfo
+            | :padding
+            | :application
+            | :seektable
+            | :vorbis_comment
+            | :cuesheet
+            | :picture
+        }
+
+  @enforce_keys [:type]
+
+  defstruct @enforce_keys
+end
