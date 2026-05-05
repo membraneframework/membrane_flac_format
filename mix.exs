1c4cc33b7a9649c7637495d472f313e199fbdaaa
Piotr Szeremeta <piotrek.szeremeta@swmansion.com>
2026-05-05 14:11:14 +0200

    Bump version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.FLAC.Mixfile do
   use Mix.Project
 
-  @version "0.2.0"
+  @version "0.2.1"
   @github_url "https://github.com/membraneframework/membrane_flac_format"
 
   def project do
